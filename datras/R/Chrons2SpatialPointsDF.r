1e14dee6b2cb94f28b6bf76ff46fe07f1f9d4626
doug.beare@gmail.com <doug.beare@gmail.com@39c3a66a-196d-608d-d44b-9f6a7deadb80>
2011-09-21 06:24:06 +0000

    Create Spatial Points Data Frame

---
+++ datras/R/Chrons2SpatialPointsDF.r
@@ -0,0 +1,13 @@
+
+### Create a SpatialPointsDataFrame from chrons (see package: sp)
+
+chrons2SpatialPointsDF <- function(data=chrons) {
+coords  <- coordinates(chrons[c("shootlong","shootlat")])
+#-Coords can be turned into spatial points and if you add a data.frame to it, to a SpatialPoints dataframe
+sp      <- SpatialPoints(coords)
+spDF    <- SpatialPointsDataFrame(coords,data=data.frame(year=chrons$year,month=chrons$month,day=chrons$day,ship=chrons$ship,timeshot=chrons$timeshot,
+gear=chrons$gear,haulduration=chrons$hauldur))
+#-Add a projection attribute
+proj4string(spDF) <- CRSargs(CRS("+init=epsg:4326"))
+spDF
+}
